da929d42a840d4d537f6e29d1b16c37daec3a8f9
Fredev7/ <elfreigod@gmail.com>
2026-05-21 22:42:11 -0500

    cambio en settings para el email

--- backend/settings.py
+++ backend/settings.py
@@ -195,14 +195,13 @@ LOGOUT_REDIRECT_URL = 'login'
 
 # Configuracion de correo para solicitudes de recuperacion de contraseña
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.gmail.com")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
-EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS", "True") == "True"
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
-ADMIN_RECOVERY_EMAIL = os.environ.get("ADMIN_RECOVERY_EMAIL", EMAIL_HOST_USER)
-
+EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
+EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default=EMAIL_HOST_USER)
+ADMIN_RECOVERY_EMAIL = config("ADMIN_RECOVERY_EMAIL", default=EMAIL_HOST_USER)
 EMAIL_TIMEOUT = 10
 
 # Default primary key field type
